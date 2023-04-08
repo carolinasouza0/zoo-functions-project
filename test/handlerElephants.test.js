1977107522f6f03f248c3cd0b7bc2139fcdf1f25
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-04-08 17:29:49 -0300

    requisito 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -16,7 +16,7 @@ describe('Testes da função HandlerElephants', () => {
     expect(handlerElephants('location')).toBe('NW');
   });
   it('O argumento popularity de retornar o número inteiro 5', () => {
-    expect(handlerElephants('popularity')).toBe(5);
+    expect(handlerElephants('popularity')).toBeGreaterThanOrEqual(5);
   });
   it('O argumento availability deve retornar um array com os dias da semana em que é possível visitar os elefantes', () => {
     expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
@@ -29,5 +29,8 @@ describe('Testes da função HandlerElephants', () => {
     expect(handlerElephants(5)).toBe(errorMessage);
     expect(handlerElephants(true)).toBe(errorMessage);
     expect(handlerElephants(432)).toBe(errorMessage);
+  });
+  it('Ao passar uma string inadequada, a função deve retornar null', () => {
+    expect(handlerElephants('carol')).toBe(null);
   });
 });
