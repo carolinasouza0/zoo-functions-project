d376c25a434faccdfda44691f725a37becfc0931
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-04-08 11:48:04 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  let animal = [];
+  if (ids.length === 0) {
+    animal = [];
+  } else {
+    ids.forEach((id) => {
+      animal.push(data.species.find((animals) => animals.id === id));
+    });
+  }
+  return animal;
 };
-
 module.exports = getSpeciesByIds;
