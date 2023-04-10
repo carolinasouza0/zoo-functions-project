5ccbee9b30b13bb28c1a19907e3d6bc516fc0ddd
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-04-10 20:08:48 -0300

    requisito 11 - começo

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,55 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const getEmployee = (obj) => {
+  const employee = data.employees.find((worker) => worker.firstName === obj.name
+      || worker.lastName === obj.name
+      || worker.id === obj.id);
+
+  if (!employee) {
+    throw new Error('Informações inválidas');
+  }
+  return employee;
+};
+
+const getSpecies = (employee) => {
+  const animals = [];
+  const employeeResponsible = getEmployee(employee).responsibleFor;
+  data.species.forEach((species) => {
+    if (employeeResponsible.includes(species.id)) {
+      animals.push(species.name);
+    }
+  });
+  return animals;
+};
+
+const getLocation = (employee) => {
+  const location = [];
+  const animals = getSpecies(employee);
+  data.species.forEach((species) => {
+    if (animals.includes(species.name)) {
+      location.push(species.location);
+    }
+  });
+  return location;
+};
+
+const getEmployeesCoverage = (employeeInfo) => {
+//   let objReturned = {};
+//   if (!employeeInfo) {
+//     data.employees.map((person) => {
+//       objReturned[id] = person.id;
+//       objReturned[fullName] = `${person.firstName} ${person.lastName}`;
+//       objReturned[species] = getSpecies(person);
+//       objReturned[locations] = getLocation(person);
+//     });
+//   } else {
+//     objReturned[id] = getEmployee(employeeInfo).id;
+//     objReturned[fullName] = `${getEmployee(employeeInfo).firstName} 
+//       ${getEmployee(employeeInfo).lastName}`;
+//     objReturned[species] = getSpecies(employeeInfo);
+//     objReturned[locations] = getLocation(employeeInfo);
+//   }
+//   return objReturned;
 };
 
 module.exports = getEmployeesCoverage;
