6e1e05dc1b51dd9af4dd69429cb76f23c9b90fe8
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-04-11 12:40:18 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
-};
+const { species } = data;
 
+const countAnimals = (animal = {}) => {
+  let obj = {};
+  if (animal.species && !animal.sex) {
+    obj = species.find((ani) => ani.name === animal.species).residents.length;
+  } else if (animal.species && animal.sex) {
+    obj = species.find((anim) => anim.name === animal.species).residents.filter((resident) =>
+      resident.sex === animal.sex).length;
+  } else {
+    species.forEach((specie) => {
+      obj[specie.name] = specie.residents.length;
+    });
+  }
+  return obj;
+};
 module.exports = countAnimals;
