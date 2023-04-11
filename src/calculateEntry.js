d0a27e5b75eef20f8f53ebef8d278e35d8fbbbb5
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-04-11 13:51:46 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const obj = {
+    child: entrants.filter((person) => person.age < 18).length,
+    adult: entrants.filter((person) => person.age >= 18 && person.age < 50).length,
+    senior: entrants.filter((person) => person.age >= 50).length,
+  };
+  return obj;
 };
-
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  let sum = 0;
+  if (entrants) {
+    const { child, adult, senior } = countEntrants(entrants);
+    sum = (child * 20.99) + (adult * 49.99) + (senior * 24.99);
+  } else {
+    sum = 0;
+  }
+  return sum;
 };
 
 module.exports = { calculateEntry, countEntrants };
