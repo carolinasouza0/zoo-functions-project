2b2a514db626c6fbfe26318cedc6e8181ddba2ea
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-04-11 17:58:15 -0300

    requisito 11 - finalmente

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,69 +2,26 @@ const data = require('../data/zoo_data');
 
 const { species, employees } = data;
 
-const getEmployee = (obj) => {
-  const employee = employees.find((worker) => worker.firstName === obj.name
-      || worker.lastName === obj.name
-      || worker.id === obj.id);
-  if (!employee) {
+const getEmployeesCoverageNoInfo = employees.map((empl) => {
+  const obj = {
+    id: empl.id,
+    fullName: `${empl.firstName} ${empl.lastName}`,
+    species: empl.responsibleFor.map((ident) => species.find((spec) => spec.id === ident).name),
+    locations: empl.responsibleFor.map((ident) => species.find((spec) =>
+      spec.id === ident).location),
+  };
+  return obj;
+});
+
+const getEmployeesCoverage = (employeeInfo) => {
+  if (!employeeInfo) return getEmployeesCoverageNoInfo;
+  const validation = getEmployeesCoverageNoInfo
+    .find((element) => element.fullName.includes(Object.values(employeeInfo))
+      || element.id.includes(Object.values(employeeInfo)));
+  if (validation === undefined) {
     throw new Error('Informações inválidas');
   }
-  return employee;
+  return validation;
 };
 
-const getSpecies = (employee) => {
-  const animals = [];
-  const employeeResponsible = getEmployee(employee).responsibleFor;
-  species.forEach((specie) => {
-    if (employeeResponsible.includes(specie.id)) {
-      animals.push(specie.name);
-    }
-  });
-  return animals;
-};
-
-const getLocation = (employee) => {
-  const location = [];
-  const animals = getSpecies(employee);
-  species.forEach((specie) => {
-    if (animals.includes(specie.name)) {
-      location.push(specie.location);
-    }
-  });
-  return location;
-};
-
-// const getEmployeesCoverageNoInfo = () => {
-//   const allEmployes = [];
-//   employees.forEach((emp) => {
-//     const animals = getSpecies(emp.id);
-//     const locations = getLocation(emp.id);
-//     allEmployes.push({
-//       id: emp.id,
-//       fullName: `${emp.firstName} ${emp.lastName}`,
-//       species: animals,
-//       locations,
-//     });
-//   });
-//   return allEmployes;
-// };
-
-const getEmployeesCoverage = (employeeInfo = {}) => {
-  if (employeeInfo) {
-    const worker = getEmployee(employeeInfo);
-    const { id } = worker;
-    const specie = getSpecies(employeeInfo);
-    const locations = getLocation(employeeInfo);
-    return {
-      id,
-      fullName: `${worker.firstName} ${worker.lastName}`,
-      species: specie,
-      locations,
-    };
-  // } else {
-  //   return getEmployeesCoverageNoInfo();
-  // }
-  }
-};
-console.log(getEmployeesCoverage({ name: 'Spry' }));
 module.exports = getEmployeesCoverage;
